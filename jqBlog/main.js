795d4d1b29af0dd1fab25396768994f0948ad195
Nathan Hall <nathan.mh@gmail.com>
2016-02-17 12:21:45 -0500

    basic jquery blog with click events

---
+++ jqBlog/main.js
@@ -0,0 +1,62 @@
+appendStrToElement(constructBlogHtml(),$('section'));
+
+
+function getBlogPosts(existingBlogPosts) {
+  return existingBlogPosts;
+}
+
+function constructBlogHtml() {
+  var posts = getBlogPosts(blogPosts);
+  var giantStr = "";
+  giantStr = posts.map(createBlogPostStr);
+  return giantStr;
+}
+
+function createBlogPostStr(post,idx) {
+  var postsHtmlStr = "";
+  postsHtmlStr += "<ul><li><h3>"+ parseInt((idx + 2)) +"  </h3><h3>).  Title: " + post.title + "</h3></li>";
+  postsHtmlStr += "<li><p>" + post.body + "</p></li>";
+  postsHtmlStr += "<li><h6>" + post.author + "</h6></li>";
+  postsHtmlStr += "</ul>"
+  return postsHtmlStr;
+}
+
+function appendStrToElement(htmlStr, $element) {
+  return $element.append(htmlStr);
+};
+
+function grabPostSubmission() {
+  var author = $($('input[type="text"]').last()[0]).val();
+  var body = $('textarea').val();
+  var title = $($('input[type="text"]').first()[0]).val();
+  $($('input[type="text"]').last()[0]).val("")
+  $('textarea').val("");
+  $($('input[type="text"]').first()[0]).val("");
+  return {
+    author: author,
+    body: body,
+    title: title,
+  }
+};
+
+$('input[type="submit"]').click(function(event) {
+  event.preventDefault();
+  var post = grabPostSubmission();
+  var blogStr = createBlogPostStr(post,4)
+  appendStrToElement(blogStr,$('section'));
+  return
+})
+
+$('header li:first-child').click(function(event) {
+  event.preventDefault();
+  $about = $('#about-page');
+  $blogs = $('#blogs');
+  if($about.hasClass('dont-show-me')) {
+    $about.removeClass('dont-show-me');
+    $blogs.addClass('dont-show-me');
+  } else {
+    $blogs.removeClass('dont-show-me');
+    $about.addClass('dont-show-me');
+  }
+
+});
