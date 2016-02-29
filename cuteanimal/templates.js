d4951e2167ac3236f93b910cf7061acd32183d61
Nathan Hall <nathan.mh@gmail.com>
2016-02-29 10:59:52 -0500

    review of using ajax and external data

---
+++ cuteanimal/templates.js
@@ -0,0 +1,8 @@
+var templates = {
+  cuteanimal: [
+    "<div class='animal'>",
+    '<p class="text"> <span class="votes"> <%= score %> </span>',
+    "<%= title %> </p>",
+    "<img src='<%= imgUrl %>' /></div>"
+  ].join('')
+}
