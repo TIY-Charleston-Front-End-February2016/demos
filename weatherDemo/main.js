d62d53e6d5e22d9305a02ae90bef1f0bd1649d57
Calvin Webster <calweb@gmail.com>
2016-02-29 15:18:33 -0500

    just some cleanup and some comments

--- weatherDemo/main.js
+++ weatherDemo/main.js
@@ -8,26 +8,27 @@ var page = {
   },
   initStyling: function () {},
   initEvents: function () {},
-  // apiKey: "146bb637c258a60ddff2af888dd17031",
   apiKey: "302367002e0604563f3f122a44eca752",
   getLocation: function () {
     navigator.geolocation.getCurrentPosition(page.onPosition);
   },
   onPosition: function (coordsObj) {
-    console.log(coordsObj.coords.latitude);
+    console.log('this is the object containing lat and lng: ', coordsObj);
     $.ajax({
       url: page.buildForcastUrl(coordsObj.coords),
       method: "GET",
       dataType: "jsonp",
-      success: function (res) {
+      success: function (responseFromWeatherAPI) {
 
         // here's the gold!!!  dig it :)
-        console.log('response: ',res);
+        console.log('response: ',responseFromWeatherAPI);
+        // you can use a separate function to do stuff with the data:
+        page.getForecast(responseFromWeatherAPI);
       }
     });
   },
   getForecast: function (data) {
-    console.log(data);
+    console.log('in getForecast data, and its your data, or dada dada - haha!!', data);
   },
   buildForcastUrl: function (coords) {
     return "http://api.openweathermap.org/data/2.5/weather?lat=" +coords.latitude+ "&lon=" + coords.longitude + "&appid=" + page.apiKey;
